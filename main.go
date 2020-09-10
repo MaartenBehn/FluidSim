a55997d7cfda1946216620ec0205192668aab09b
maart <maart@DESKTOP-R5T2CB9>
2020-09-10 23:21:23 +0100

    Added OctaForceEngine

--- main.go
+++ main.go
@@ -1,49 +1,24 @@
-// Copyright 2014 The go-gl Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Renders a textured spinning cube using GLFW 3 and OpenGL 4.1 core forward-compatible profile.
 package main
 
 import (
-	"FluidSim/Window"
+	"OctaForce"
 	"log"
 )
 
-func init() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-}
-
 func main() {
-	Window.Start(start)
+	OctaForce.StartUp(start, stop)
 }
 
 func start() {
-	for !Window.WindowClosed() {
-		Window.Update()
-	}
+	OctaForce.SetMaxFPS(60)
+	OctaForce.SetMaxUPS(20)
+	OctaForce.AddUpdateCallback(update)
 }
 
-// Set the working directory to the root of Go package, so that its assets can be accessed.
-/*
-func init() {
-	dir, err := importPathToDir("github.com/go-gl/example/gl41core-cube")
-	if err != nil {
-		log.Fatalln("Unable to find Go package in your GOPATH, it's needed to load assets:", err)
-	}
-	err = os.Chdir(dir)
-	if err != nil {
-		log.Panicln("os.Chdir:", err)
-	}
+func update() {
+	log.Print(OctaForce.GetFPS())
 }
 
-// importPathToDir resolves the absolute path from importPath.
-// There doesn't need to be a valid Go package inside that import path,
-// but the directory must exist.
-func importPathToDir(importPath string) (string, error) {
-	p, err := build.Import(importPath, "", build.FindOnly)
-	if err != nil {
-		return "", err
-	}
-	return p.Dir, nil
-}*/
+func stop() {
+
+}
