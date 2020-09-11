eb8921e10bffd3bf70d8bdeec5c4d9a122dabc22
maart <maart@DESKTOP-R5T2CB9>
2020-09-11 20:25:21 +0100

    Componentsystem

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"OctaForce"
+	"OctaForceEngineGo"
+	"github.com/go-gl/mathgl/mgl32"
 	"log"
 )
 
@@ -13,6 +14,19 @@ func start() {
 	OctaForce.SetMaxFPS(60)
 	OctaForce.SetMaxUPS(20)
 	OctaForce.AddUpdateCallback(update)
+
+	e1 := OctaForce.CreateEntity()
+	OctaForce.AddComponent(e1, OctaForce.COMPONENT_Transform)
+	transform1 := OctaForce.GetComponent(e1, OctaForce.COMPONENT_Transform).(OctaForce.Transform)
+	transform1.Position = mgl32.Vec3{1, 1, 1}
+
+	e2 := OctaForce.CreateEntity()
+	OctaForce.AddComponent(e2, OctaForce.COMPONENT_Transform)
+	transform2 := OctaForce.GetComponent(e2, OctaForce.COMPONENT_Transform).(OctaForce.Transform)
+	transform2.Position = mgl32.Vec3{2, 2, 2}
+
+	OctaForce.RemoveComponent(e2, OctaForce.COMPONENT_Transform)
+	OctaForce.DeleteEntity(e2)
 }
 
 func update() {
